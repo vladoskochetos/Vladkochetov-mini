2bc0918b6bd39f7f27819127816d1f449350b272
vladoskochetos <vladoskochetos@gmail.com>
2026-09-09 19:44:06 +0300

    Make robots static for GitHub Pages

--- app/robots.ts
+++ app/robots.ts
@@ -1,3 +1,11 @@
 import type { MetadataRoute } from "next";
 import { siteConfig } from "@/config/site";
-export default function robots(): MetadataRoute.Robots { return { rules: { userAgent: "*", allow: "/" }, sitemap: `${siteConfig.url}/sitemap.xml` }; }
+
+export const dynamic = "force-static";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: { userAgent: "*", allow: "/" },
+    sitemap: `${siteConfig.url}/sitemap.xml`,
+  };
+}
